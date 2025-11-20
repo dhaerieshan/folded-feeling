67baa98001371942db969cdf9bb0f30f9166b0c9
dhaerieshan <dhaerieshan@gmail.com>
2025-11-21 01:03:16 +0530

    new

--- secretproposal/secretproposal/settings.py
+++ secretproposal/secretproposal/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'proposalapp'
+    'proposalapp',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
